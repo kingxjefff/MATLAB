e00d29d575dc3f94e917828e9fe24d4328da60e3
Andrew Jefferson <58449063+kingxjefff@users.noreply.github.com>
2022-04-14 20:57:29 -0700

    today

--- EE-522/Hwk6/HWk6.m
+++ EE-522/Hwk6/HWk6.m
@@ -9,43 +9,109 @@ Gs = tf(2, [1 0.5])
 
 Gz = c2d(Gs, Ts)
 Gzcl = feedback(K*Gz, 0.04)
-%rlocus(Gz)
+
 figure(1);
 hold on;
-bode(Gzcl)
+grid on;
+bode(Gzcl);
 hold off;
 
-%% problem 3
-Ts = 0.05;
-Gs = tf(20, [1 6 0])
-Gz = c2d(Gs, Ts)
-Gzcl = feedback(Gz, 0.4)
-figure(2);
-hold on;
-bode(Gzcl)
 
-hold off;
-
-%% problem 4
+%% Problem 4
 figure(3);
 Ts = 0.001;
 delta = 1/sqrt(2);
 wo = 2*pi*60;
-hold on;
+
 Gs = tf([1 0 wo^2],[1 2*delta*wo wo^2])
-bode(Gs)
-Gz = c2d(Gs, Ts, 'tustin')
-bode(Gz)
-opt = c2dOptions('method', 'tustin', 'PrewarpFrequency', 377)
-Gz = c2d(Gs, Ts, opt)
-bode(Gz)
-Gz = c2d(Gs, Ts, 'matched')
-bode(Gz)
+
+Gz1 = c2d(Gs, Ts, 'tustin')
+
+opt = c2dOptions('method', 'tustin', 'PrewarpFrequency', 377);
+Gz2 = c2d(Gs, Ts, opt);
+
+Gz3 = c2d(Gs, Ts, 'matched')
+
 
 Gzdiff = tf([0.682 -1.194 0.597], [1 -1.512 0.597]);
-bode(Gzdiff)
+t = 0:Ts:0.1;
+% plots for problem 4
+sig = sin(2*pi*60*t)+sin(2*pi*10*t);
 
-
-
-
+% plot a
+subplot(2,2,1);
+title('Backwards Difference Approximation');
+xlabel('Time (s)');
+ylabel('Response');
+grid on;
+hold on;
+plot(t, sig);
+[n,d] = tfdata(Gzdiff);
+n = cell2mat(n);
+d = cell2mat(d);
+plot(t, filter(n,d, sig));
 hold off;
+
+% plot b
+subplot(2,2,2);
+title('Bilinear Transform');
+xlabel('Time (s)');
+ylabel('Response');
+grid on;
+hold on;
+plot(t, sig);
+[n,d] = tfdata(Gz1);
+n = cell2mat(n);
+d = cell2mat(d);
+plot(t, filter(n,d, sig));
+hold off;
+
+% plot c
+subplot(2,2,3);
+title('Bilinear Transform with pre-warping');
+xlabel('Time (s)');
+ylabel('Response');
+grid on;
+hold on;
+plot(t, sig);
+[n,d] = tfdata(Gz2);
+n = cell2mat(n);
+d = cell2mat(d);
+plot(t, filter(n,d, sig));
+hold off;
+
+% plot b
+subplot(2,2,4);
+title('Matched Pole-Zero Method');
+xlabel('Time (s)');
+ylabel('Response');
+grid on;
+hold on;
+plot(t, sig);
+[n,d] = tfdata(Gz3);
+n = cell2mat(n);
+d = cell2mat(d);
+plot(t, filter(n,d, sig));
+hold off;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
