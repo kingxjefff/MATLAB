42c04056f847c5951bcdc1a76f85fe8fb78890d3
Andrew Jefferson <58449063+kingxjefff@users.noreply.github.com>
2022-05-09 14:21:46 -0700

    modulate

--- EE-558/project.m
+++ EE-558/project.m
@@ -7,27 +7,34 @@ x = 20*log10(x);
 x1 = x.^0.5;
 x2 = (x*2).^0.5;
 
-loglog(x, qfunc(x1), x, qfunc(x2));
-title("BER Preformance of BASK and BPSK");
-ylabel("BER");
-xlabel("Eb/No (dB)");
+%loglog(x, qfunc(x1), x, qfunc(x2));
+%title("BER Preformance of BASK and BPSK");
+%ylabel("BER");
+%xlabel("Eb/No (dB)");
 
 
 %% Problem 2
-L = 10000;
+L = 10;
+n = 100;         % upsample amount
 Eb = 1;
 No = 10^-3;
+Fc = 100;       % carrier frequency
+Tc = 1/Fc;
 
+t1 = Tc:Tc:Tc*n*L;
+t1 = t1.';
+
+% random message
 BPSK = randi([0, 1], L, 1);
 BASK = randi([0, 1], L, 1);
 
-noise = randn([L 1]);
+noise = randn([L*n 1]);
 
 for i = 1:L
-    switch(BPSK(i))
-        case 0
+   switch(BPSK(i))
+       case 0
             BPSK(i) = -1;
-        case 1
+       case 1
     end
 end
 
@@ -37,13 +44,43 @@ for i = 1:L
     end
 end
 
-sig1 = BPSK + sqrt(No/2) * noise;
+% up sampling for BPSK
+BPSKuS = zeros([L*n, 1]);
+for i = 1:L
+    switch(BPSK(i))
+       case -1
+           for j = 1:n
+            BPSKuS(j + (i-1)*n) = -1;
+           end
+       case 1
+           for j = 1:n
+            BPSKuS(j + (i-1)*n) = 1;
+           end
+    end
+end
+
+% transmitter
+figure(2);
+subplot(2,2,1);
+hold on;
+plot(t1, BPSKuS);
+sig1 = BPSKuS .* cos(2*pi*t1);
+plot(t1, sig1);
+
+subplot(2,2,2);
+tsig1 = sig1 + sqrt(No/2) * noise;
+plot(t1, tsig1);
 %scatter(sig1(1:L), 1);
 
 
 % Reciever
+subplot(2,2,3);
+rsig1 = tsig1 .* cos(2*pi*t1);  % demodulation
+plot(t1, rsig1);
 
-rsig1 = zeros([L, 1]);
+subplot(2,2,4);
+rsig1 = lowpass(rsig1, 1, 10);
+plot(t1, rsig1);
 for i = 1:L
     if(sig1(i) <= 0)
         rsig1(i) = -1;
